22893e3b6104dadb30fa85318a697da22b07e099
cameronff <cameron@jferdinands.com>
2014-04-26 16:20:44 +1000

    Updates

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 print("Halt!")
-user_input = input("Who goes there? ")
-print("You may pass,",  user_input)
+user_input = input("What is your name? ")
+user_input2 = input("What is your Age? ")
+print("You may pass,",  user_input, "because you are", user_input2)
