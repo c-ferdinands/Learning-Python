0541efc206151fbf082aee695aeaf2d6583c7de1
cameronff <cameron@jferdinands.com>
2014-04-27 13:44:54 +1000

    Progress!

--- example.py
+++ example.py
@@ -1,6 +1,53 @@
 #!/usr/bin/python3
 
-print("Halt!")
-user_input = input("What is your name? ")
-user_input2 = input("What is your Age? ")
-print("You may pass,",  user_input, "because you are", user_input2)
+# password = str()
+# while password != "unicorn":
+#     password = input("Password: ")
+# print("Continues...")
+# 
+# print("Halt!")
+# user_input = input("What is your name? ")
+# user_input2 = input("What is your Age? ")
+# print("You may pass,",  user_input, "because you are", user_input2)
+# 
+# 
+# ""
+# a = 1
+# s = 0
+# print('Enter Numbers to add to the sum.')
+# print('Enter 0 to quit.')
+# while a != 0:
+#     print('Current Sum:', s)
+#     a = float(input('Number? '))
+#     s = s + a
+# ""
+# 
+# b = 0
+# c = 0
+# while c < 10:
+#     c = c + 1
+#     print(user_input2)
+# 
+# name = input('Set name: ')
+# password = input('Set password: ')
+# while 1 == 1:
+#     nameguess=""
+#     passwordguess=""
+#     key=""
+#     while (nameguess != name) or (passwordguess != password):
+#         nameguess = input('Name? ')
+#         passwordguess = input('Password? ')
+#     print("Welcome,", name, ". Type lock to lock.")
+#     while key != "lock":
+#         key = input("")
+
+while 1 == 1:
+    name = input('Your name: ')
+    if name == 'John1':
+        print('That is a nice name.')
+    elif name == 'Jim':
+        print('... some funny text.')
+    elif name == 'John':
+        print('... some funny text.')
+    else:
+        print('You have a nice name.')
