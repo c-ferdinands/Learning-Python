5de6c9f1b704fc88f16c47827298c5d0c1c1841a
cameronff <cameron@jferdinands.com>
2014-04-28 21:22:19 +1000

    Genderrrzzz

--- example1.py
+++ example1.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python3
 
-onetoten = range(1, 11)
-for item in onetoten:
-    print(item)
+#onetoten = range(1, 11)
+#for item in onetoten:
+#    print(item)
+#
+#list = [4, 5, 7, 8, 9, 1, 0, 7, 10]
+#list.sort()
+#print(list)
+#prev = None
+#for item in list:
+#    if prev == item:
+#        print("Duplicate of", prev, "found")
+#        print("Removing", prev)
+#        list.remove(prev)
+#    prev = item
+#print(len(list))
+#
+#print(prev >= 10)
 
-list = [4, 5, 7, 8, 9, 1, 0, 7, 10]
-list.sort()
-print(list)
-prev = None
-for item in list:
-    if prev == item:
-        print("Duplicate of", prev, "found")
-        print("Removing", prev)
-        list.remove(prev)
-    prev = item
-print(list)
+
+
+#for value in attendees:
+#    print(value)
+
+def genders():
+    attendees = ['Male', 'Female', 'Female', 'Male']
+    male_count = 0
+    female_count = 0
+    while male_count != 2:
+        for value in attendees:
+            if value == 'Male':
+                male_count = male_count + 1
+            else:
+                female_count = female_count + 1
+    print("Males:", male_count)
+    print("Females:", female_count)
+genders()
