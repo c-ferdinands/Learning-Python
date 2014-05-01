090194ea538de1c368ae0a3e2c5994c9b93efa2a
cameronff <cameron@jferdinands.com>
2014-05-01 21:20:01 +1000

    Fucks given = 10000

--- example1.py
+++ example1.py
@@ -23,20 +23,64 @@
 #for value in attendees:
 #    print(value)
 
-#def genders():
-#    attendees = ['Male', 'Female', 'Female', 'Male']
-#    male_count = 0
-#    female_count = 0
-#    while male_count != 2:
-#        for value in attendees:
-#            if value == 'Male':
-#                male_count = male_count + 1
-#            else:
-#                female_count = female_count + 1
-#    print("Males:", male_count)
-#    print("Females:", female_count)
-#genders()
 
-attendees = ['Male', 'Female', 'Female', 'Male']
+# def genders():
+#     attendees = ['Male', 'Female', 'Female', 'Male']
+#     male_count = 0
+#     female_count = 0
+#     current_count = 0
+#     while current_count < len(attendees):
+#         for value in attendees:
+#             if value == 'Male':
+#                 male_count = male_count + 1
+#                 current_count = current_count + 1
+#             else:
+#                 female_count = female_count + 1
+#                 current_count = current_count + 1
+#     else:
+#         print("Males:", male_count)
+#         print("Females:", female_count)
+# genders()
 
-print(attendees[1])
+#attendees = ['Male', 'Female', 'Female', 'Male']
+#
+#print(attendees[1])
+
+# name = raw_input("What is your name? ")
+# password = raw_input("What is the password? ")
+# if name == "Josh" and password == "Friday":
+#     print("Welcome Josh")
+# elif name == "Fred" and password == "Rock":
+#     print("Welcome Fred")
+# else:
+#     print("I don't know you.")
+#numbers = {}
+#print("Add Name and Number")
+#name = input("Name: ")
+#phone = input("Number: ")
+#numbers[name] = phone
+#for x in numbers.keys():
+#    print("Name: ", x, "\tNumber:", numbers[x])
+#
+#
+#       # print("Add Name and Number")
+#       # name = input("Name: ")
+#       # phone = input("Number: ")
+#       # numbers[name] = phone
+#
+#
+#fucks = {}
+#who = input("Who is giving the fucks? ")
+#given = input("how many fucks are given? ")
+#fucks[who] = given
+#for x in fucks.keys():
+#    print(who, "\tgives", given, "fucks")
+
+
+numbers = {}
+print("Add Name and Number")
+name = input("Name: ")
+phone = input("Number: ")
+numbers[name] = phone
+for x in numbers.keys():
+    print("Name: ", x, "\tNumber:", numbers[x])
