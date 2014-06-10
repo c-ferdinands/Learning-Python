3115789d138cf232df967b89f19b8cf0b5c4d14e
cameronff <cameron@Camerons-MacBook-Air.local>
2014-06-10 20:28:04 +1000

    File I/O is awesome. Not a lot of work, but it's about forming a habit

--- fucks.py
+++ fucks.py
@@ -1,6 +1,6 @@
 def fucks():
     fucks = {}
-    who = input("Who is giving the fucks? ")
+    who = raw_input("Who is giving the fucks? ")
     given = input("how many fucks are given? ")
     fucks[who] = given
     for x in fucks.keys():
---
+++ next.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+numbers = [0,1,2,3,4,5,2,4,3,43,43,2,432,3,1]
+print numbers[9:14]
+
+numbersx2 = numbers * 2
+
+print numbersx2[0:]
+
+print numbers[:]
+
+with open("test.txt", "wt") as out_file:
+    out_file.write("Text!")
+
+import subprocess
+def main():
+    subprocess.call(['ls'])
+main()
