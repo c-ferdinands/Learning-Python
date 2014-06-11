1580a9306ea4fc3cc3e3902cf661c29d8d02f487
c-ferdinands <cameron@Camerons-iMac.local>
2014-06-11 21:36:50 +1000

    New day, new code!

--- next.py
+++ next.py
@@ -1,18 +1,39 @@
 #!/usr/bin/python3
 
-numbers = [0,1,2,3,4,5,2,4,3,43,43,2,432,3,1]
-print numbers[9:14]
+#numbers = [0,1,2,3,4,5,2,4,3,43,43,2,432,3,1]
+#print numbers[9:14]
 
-numbersx2 = numbers * 2
+#numbersx2 = numbers * 2
 
-print numbersx2[0:]
+#print numbersx2[0:]
+#
+#print numbers[:]
+#
+#with open("test.txt", "wt") as out_file:
+#    out_file.write("Text!")
+#
+#import subprocess
+#def main():
+#    subprocess.call(['ls', '/'])
+#main()
+#
+#
+#import copy
+#a = [[1,2,3,],[4,5,6]]
+#b = a[:]
+#b[1][2] = 40
+##c[1][1] = 90
+#print(a)
+#print(b)
+#
+print("Type Control C or -1 to exit")
+number = 1
+while number != -1:
+    try:
+        number = int(input("Enter a number: "))
+        print("You entered:", number)
+    except ValueError:
+        print("That was not a number.")
 
-print numbers[:]
 
-with open("test.txt", "wt") as out_file:
-    out_file.write("Text!")
 
-import subprocess
-def main():
-    subprocess.call(['ls', '/'])
-main()
